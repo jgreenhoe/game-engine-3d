3e68d9d00d565415073c03c8ed9ba0cc7ad87ed3
jgreenhoe <greenhoejon@gmail.com>
2024-02-26 20:30:19 -0500

    added multi-object support and object position, primitive collision detection

--- draw_loop.c
+++ draw_loop.c
@@ -3,12 +3,14 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 
-void draw_polygon(SDL_Window* screen, SDL_Renderer* renderer, int ROW,int COL,int polygon[][2][COL]);
+void draw_polygon(SDL_Window* screen, SDL_Renderer* renderer, int ROW,int COL,float polygon_float[][2][COL], float colors_float[][3]);
 SDL_Window* create_SDL_window(const char* title, int x, int y, int width, int height, Uint32 flags);
 SDL_Renderer* create_SDL_renderer(SDL_Window* screen, int index, Uint32 flags);
+void clear_SDL_renderer(SDL_Renderer* renderer);
+void present_SDL_renderer(SDL_Renderer* renderer);
 
 int main(){
-    int polygon[4][2][4] = {
+    float polygon[4][2][4] = {
     {{100, 450, 450, 100},
     {100, 100, 200, 200}},
 
@@ -21,12 +23,19 @@ int main(){
     {{550, 900, 900, 550},
     {300, 300, 400, 400}}
     };
+    float colors[4][3] = {
+    {255, 255, 255},
+    {255, 0, 0},
+    {0, 255, 0},
+    {0, 0, 255}
+    };
     int ROW = sizeof(polygon)/sizeof(polygon[0]);
     int vertex_count = sizeof(polygon[0][0])/sizeof(polygon[0][0][0]);
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* screen = create_SDL_window("Game Base", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1915, 1000, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = create_SDL_renderer(screen, -1, 0);
-    draw_polygon(screen, renderer, ROW, vertex_count, polygon);
+    draw_polygon(screen, renderer, ROW, vertex_count, polygon, colors);
+    present_SDL_renderer(renderer);
     SDL_Delay(1000);
 }
 SDL_Window* create_SDL_window(const char* title, int x, int y, int width, int height, Uint32 flags){
@@ -38,24 +47,21 @@ SDL_Renderer* create_SDL_renderer(SDL_Window* screen, int index, Uint32 flags){
     SDL_Renderer* renderer = SDL_CreateRenderer(screen, -1, 0);
     return renderer;
 }
-
-void draw_polygon(SDL_Window* screen, SDL_Renderer* renderer, int ROW,int COL,int polygon_int[][2][COL]){
-    int screen_width, screen_height;
-    SDL_GetWindowSize(screen, &screen_width, &screen_height);
-    Sint16 polygon[ROW][2][COL];
-    for (int i = 0; i < ROW; i++){
-        for (int j = 0; j < 2; j++){
-            for (int k = 0; k < COL; k++){
-                polygon[i][j][k] = (Sint16) polygon_int[i][j][k];
-            }
-        }
-    }
+    
+void clear_SDL_renderer(SDL_Renderer* renderer){
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
-    float r = 0;
-    float g = 0;
-    float b = 0;
-    float increment = (float) 255/ROW;
+}
+    
+void present_SDL_renderer(SDL_Renderer* renderer){
+    SDL_RenderPresent(renderer);
+}
+
+void draw_polygon(SDL_Window* screen, SDL_Renderer* renderer, int ROW, int COL, float polygon[][2][COL], float colors[][3]){
+    int screen_width, screen_height;
+    SDL_GetWindowSize(screen, &screen_width, &screen_height);
+    
+    Sint16 polygon_Sint[ROW][2][COL];
     for(int i = 0; i<ROW; i++){
         int num_vertex = sizeof(polygon[i][0])/sizeof(polygon[i][0][0]);
         int vertex_shown = num_vertex;
@@ -64,16 +70,13 @@ void draw_polygon(SDL_Window* screen, SDL_Renderer* renderer, int ROW,int COL,in
                 vertex_shown--;
             }
         }
+        for(int j = 0; j<2; j++){
+            for(int k = 0; k < COL; k++){
+                polygon_Sint[i][j][k] = (Sint16) polygon[i][j][k];
+            }
+        }
         if(vertex_shown > 0){
-            filledPolygonRGBA(renderer, polygon[i][0], polygon[i][1], COL, r,g,b, 255);
-            r += increment;
-            g += increment;
-            b += increment;
+            filledPolygonRGBA(renderer, polygon_Sint[i][0], polygon_Sint[i][1], COL, colors[i][0], colors[i][1], colors[i][2], 255);
         }
     }
-    SDL_RenderPresent(renderer);
-//    SDL_Delay(1000);
-    
-//    SDL_DestroyWindow(screen);
-//    SDL_Quit();
 }
--- gamebase.py
+++ gamebase.py
@@ -75,7 +75,30 @@ class read_mtl:
                 if line[:6] == "newmtl":
                     pass
 
-class shapes:
+#class of the camera position and rotation
+class position:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.z = -10
+        self.cam_x = 0
+        self.cam_y = 0
+        self.cam_z = 0
+        self.pos = [self.x, self.y, self.z]
+        self.cam = [self.cam_x, self.cam_y, self.cam_z]
+    def update(self):
+        self.pos = [self.x, self.y, self.z]
+        self.cam = [self.cam_x, self.cam_y, self.cam_z]
+    def right(self,a,cam_y):
+        self.x += math.cos(cam_y)*a
+        self.z -= math.sin(cam_y)*a
+    def forw(self,a,cam_y):
+        self.x += math.sin(cam_y)*a
+        self.z += math.cos(cam_y)*a
+    def up(self,a):
+        self.y += a
+
+class objects(position):
     def __init__(self,obj_file):
         obj = read_obj(obj_file)
         self.points = np.array(obj.v)
@@ -96,6 +119,60 @@ class shapes:
         #convert vertex and vertex normal faces to np.array
         self.vfaces = np.array(obj.fv)
         self.nvfaces = np.array(obj.fvn)
+        max_color = 2200
+        self.colors = np.array([np.linspace(0,75,max_color),np.full(max_color, 100),np.full(max_color, 100)])
+        
+        self.hitbox = self.Hitbox(self)
+        
+        super().__init__()
+        self.points[:,0] += self.x
+        self.points[:,1] += self.y
+        self.points[:,2] += self.z
+        self.orig_points = self.points
+        
+    def update(self):
+        self.pos = [self.x, self.y, self.z]
+        self.cam = [self.cam_x, self.cam_y, self.cam_z]
+        self.points = self.orig_points + self.pos
+        
+        
+    class Hitbox:
+        def __init__(self, objects):
+            vert_dist = np.linalg.norm(objects.points[0])
+            xbound = [objects.points[0][0], objects.points[0][0]]
+            ybound = [objects.points[0][1], objects.points[0][1]]
+            zbound = [objects.points[0][2], objects.points[0][2]]
+            self.sphere_bound_index = 0
+            self.xbound_index = [0, 0]
+            self.ybound_index = [0, 0]
+            self.zbound_index = [0, 0]
+            index_counter = 0
+            for i in objects.points:
+                if np.linalg.norm(i) > vert_dist:
+                    vert_dist = np.linalg.norm(i)
+                    self.sphere_bound_index = index_counter
+                    
+                if i[0] < xbound[0]:
+                    xbound[0] = i[0]
+                    self.xbound_index[0] = index_counter
+                elif i[0] > xbound[1]:
+                    xbound[1] = i[0]
+                    self.xbound_index[1] = index_counter
+                if i[1] < ybound[0]:
+                    ybound[0] = i[1]
+                    self.ybound_index[0] = index_counter
+                elif i[1] > ybound[1]:
+                    ybound[1] = i[1]
+                    self.ybound_index[1] = index_counter
+                if i[2] < zbound[0]:
+                    zbound[0] = i[2]
+                    self.zbound_index[0] = index_counter
+                elif i[2] > zbound[1]:
+                    zbound[1] = i[2]
+                    self.zbound_index[1] = index_counter
+                index_counter += 1
+
+                
     #multiplies self.shape by size multiplier
     def set_size(self,size):
         #size format: [x, y, z]
@@ -103,52 +180,9 @@ class shapes:
             i[0] = i[0]*size[0]
             i[1] = i[1]*size[1]
             i[2] = i[2]*size[2]
-    #takes variables to be used in tilt()
-    def set_tilt(self,point,theta):
-        self.point = point
-        self.theta = theta
-        self.ttheta = copy.deepcopy(theta)
-    #rotates self.coord by variable from set_tilt()
-    def tilt(self):
-        rotate(self.coord,self.point,self.theta)
-
-#class of the camera position and rotation
-class position:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.z = -10
-        self.vel_x = 0
-        self.vel_y = 0
-        self.vel_z = 0
-        self.cam_x = 0
-        self.cam_y = 0
-        self.cam_z = 0
-        self.rt_x = 0
-        self.rt_y = 0
-        self.rt_z = 0
-        self.pos = [self.x, self.y, self.z]
-        self.cam = [self.cam_x, self.cam_y, self.cam_z]
-    def update(self):
-        self.x = self.x + self.vel_x
-        self.y = self.y + self.vel_y
-        self.z = self.z + self.vel_z
-        self.cam_x = self.cam_x + self.rt_x
-        self.cam_y = self.cam_y + self.rt_y
-        self.cam_z = self.cam_z + self.rt_z
-        self.pos = [self.x, self.y, self.z]
-        self.cam = [self.cam_x, self.cam_y, self.cam_z]
-    def right(self,a,cam_y):
-        self.x += math.cos(cam_y)*a
-        self.z -= math.sin(cam_y)*a
-    def forw(self,a,cam_y):
-        self.x += math.sin(cam_y)*a
-        self.z += math.cos(cam_y)*a
-    def up(self,a):
-        self.y += a
 
 #create instance of position for player position
-position = position()
+cam_position = position()
 #calculates optimized 2d coordinates from 'shapes' instances,
 #then calls the draw_loop function from drawPolygon to render
 def draw_points(obj):
@@ -156,9 +190,10 @@ def draw_points(obj):
     normals = obj.normals
     faces = obj.vfaces
     nvfaces = obj.nvfaces
+    colors = obj.colors
     
-    rotated_points = rotate(points,position.pos,position.cam)
-    rotated_normals = rotate(normals, [0,0,0],position.cam)
+    rotated_points = rotate(points,cam_position.pos,cam_position.cam)
+    rotated_normals = rotate(normals, [0,0,0],cam_position.cam)
     #rotated_points format: [[x, y, z], [x, y, z]...]
 
     #filter in only the faces that contain vertices whose normals face the camera
@@ -168,13 +203,13 @@ def draw_points(obj):
     faces = faces[bool_vertices_drawn]
     
     #filter in only the faces in front of the camera
-    bool_vertices_drawn = rotated_points[:,2] > position.z
+    bool_vertices_drawn = rotated_points[:,2] > cam_position.z
     bool_polygons_drawn = bool_vertices_drawn[faces-1]
     bool_faces_drawn = np.any(bool_polygons_drawn,1)
     faces = faces[bool_faces_drawn]
     
     #compute array of 2d coordinates from rotated_points
-    flat = np.array((d*(rotated_points[:,0]-position.pos[0])/(rotated_points[:,2]-position.pos[2])+screen[0]/2,-d*(rotated_points[:,1]-position.pos[1])/(rotated_points[:,2]-position.pos[2])+screen[1]/2))
+    flat = np.array((d*(rotated_points[:,0]-cam_position.pos[0])/(rotated_points[:,2]-cam_position.pos[2])+screen[0]/2,-d*(rotated_points[:,1]-cam_position.pos[1])/(rotated_points[:,2]-cam_position.pos[2])+screen[1]/2))
     #flat format: [[x, x...], [y, y...]]
 
     #reformat arrays to pass into draw_loop
@@ -187,15 +222,21 @@ def draw_points(obj):
     
     #organize polygons by distance to camera position
     #shapes farthest away are drawn first
-    sort_points = np.linalg.norm(rotated_points[faces_array[:,0]-1]-position.pos,axis=1)
+    sort_points = np.linalg.norm(rotated_points[faces_array[:,0]-1]-cam_position.pos,axis=1)
     sort_index = np.argsort(sort_points)
     shaped_points = shaped_points[np.flip(sort_index)]
     shaped_points = np.ascontiguousarray(np.swapaxes(shaped_points,1,2))
+    
+    #configure color data
+    colors = np.swapaxes(colors,0,1)
+    colors = colors[np.flip(sort_index)]
 
     #prepare pointer of shaped_points compatible with draw_loop
-    shaped_points = shaped_points.astype(np.int32)
-    shaped_points_ptr = shaped_points.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
-    draw_loop(window,renderer,shaped_points.shape[0],shaped_points.shape[2],shaped_points_ptr)
+    shaped_points = shaped_points.astype(np.float32)
+    shaped_points_ptr = shaped_points.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    colors = colors.astype(np.float32)
+    colors_ptr = colors.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    draw_loop(window,renderer,shaped_points.shape[0],shaped_points.shape[2],shaped_points_ptr, colors_ptr)
 
 #rotates shape around points by theta
 #shape format [[x,y,z],[x,y,z]...] point format [x,y,z] theta format [x,y,z]
@@ -227,22 +268,10 @@ def rotate(shape,point,theta):
     
     return sc
 
-#clips polygon arrays to only include those visible to the camera
-def clip(polygon_o):
-    polygon = np.copy(polygon_o)
-    #polygon format: [[x, y], [x, y]...]
-    if np.all(polygon[:,0] < -screen[0]/2) or np.all(polygon[:,0] > screen[0]/2):
-        return []
-    elif np.all(polygon[:,1] < -screen[1]/2) or np.all(polygon[:,1] > screen[1]/2):
-        return []
-    elif np.all(polygon[:,0] > -screen[0]/2) and np.all(polygon[:,0] < screen[0]/2):
-        return polygon
-    elif np.all(polygon[:,1] > -screen[1]/2) and np.all(polygon[:,1] < screen[1]/2):
-        return polygon
-
 #creating and specifying objects
-#teapot = shapes("newell_teaset/teapot.obj")
-teapot = shapes("/home/pi/Downloads/sphere.obj")
+teapot = objects("newell_teaset/teapot.obj")
+player = objects("/home/pi/Downloads/sphere.obj")
+#teapot = objects("/home/pi/Downloads/Gun.obj")
 #changes the velocity and cam angle of player
 def move(keys_pressed):
     keys_released = []
@@ -256,29 +285,42 @@ def move(keys_pressed):
     
     for i in keys_pressed:
         if (i) == 'd':
-            position.right(speed,position.cam_y)
+            cam_position.right(speed,cam_position.cam_y)
         if (i) == 'a':
-            position.right(-speed,position.cam_y)
+            cam_position.right(-speed,cam_position.cam_y)
         if (i) == 's':
-            position.forw(-speed,position.cam_y)
+            cam_position.forw(-speed,cam_position.cam_y)
         if (i) == 'w':
-            position.forw(speed,position.cam_y)
+            cam_position.forw(speed,cam_position.cam_y)
         if (i) == keyboard.Key.space:
-            position.up(speed)
+            cam_position.up(speed)
         if (i) == keyboard.Key.shift:
-            position.up(-speed)
+            cam_position.up(-speed)
         
         if (i) == keyboard.Key.right:
-            position.cam_y += 0.1
+            cam_position.cam_y += 0.1
         if (i) == keyboard.Key.left:
-            position.cam_y -= 0.1
+            cam_position.cam_y -= 0.1
         if (i) == keyboard.Key.up:
-            position.cam_x += 0.1
+            cam_position.cam_x += 0.1
         if (i) == keyboard.Key.down:
-            position.cam_x -= 0.1
+            cam_position.cam_x -= 0.1
+            
+        if cam_position.cam_x >= 1.57:
+            cam_position.cam_x = 1.57
+        if cam_position.cam_x <= -1.57:
+            cam_position.cam_x = -1.57
+        if cam_position.cam_y > 6.24:
+            cam_position.cam_y = 0
+        if cam_position.cam_y < -6.24:
+            cam_position.cam_y = 0
     
     return keys_pressed
    
+def detect_hitbox(obj1, obj2):
+    dist = np.linalg.norm(obj1.points[obj1.hitbox.sphere_bound_index]-obj2.points[obj2.hitbox.sphere_bound_index])
+    print(dist)
+
 create_SDL_window = drawPolygon.create_SDL_window
 create_SDL_window.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_uint]
 create_SDL_window.restypes = ctypes.c_void_p
@@ -287,8 +329,16 @@ create_SDL_renderer = drawPolygon.create_SDL_renderer
 create_SDL_renderer.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_uint]
 create_SDL_renderer.restypes = ctypes.c_void_p
 
+clear_SDL_renderer = drawPolygon.clear_SDL_renderer
+clear_SDL_renderer.argtypes = [ctypes.c_void_p]
+clear_SDL_renderer.restypes = None
+
+present_SDL_renderer = drawPolygon.present_SDL_renderer
+present_SDL_renderer.argtypes = [ctypes.c_void_p]
+present_SDL_renderer.restypes = None
+
 draw_loop = drawPolygon.draw_polygon
-draw_loop.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_int,ctypes.c_int, ctypes.POINTER(ctypes.c_int)]
+draw_loop.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_int,ctypes.c_int, ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float)]
 draw_loop.restypes = None
 
 window = create_SDL_window(b"Game Base", 0, 0, screen[0], screen[1], 0)
@@ -303,13 +353,21 @@ def main():
     while True:
         start = time.time()
         keys_pressed = move(keys_pressed)
-        position.update()
+        cam_position.update()
+        player.x = cam_position.x
+        player.y = cam_position.y - 20
+        player.z = cam_position.z + 70
+        player.update()
+        detect_hitbox(teapot, player)
+        clear_SDL_renderer(renderer)
         draw_points(teapot)
+        draw_points(player)
+        present_SDL_renderer(renderer)
         end = time.time()
         #control framerate
         if end-start < .017:
             time.sleep(.017-(end-start))
-        print(end-start)
+        #print(end-start)
 
 if __name__ == "__main__":
     main()
